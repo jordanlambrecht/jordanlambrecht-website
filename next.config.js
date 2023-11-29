b360296beb098930255584a6da6fcb63b1f104b5
Jordan Lambrecht <jordan@pixelbakery.com>
2023-11-29 17:09:34 -0600

    feat: added redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/filament-library",
+        destination:
+          "https://jordanlambrecht.notion.site/Filaments-f9903bf00c8b47e686cafb96b465340b",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

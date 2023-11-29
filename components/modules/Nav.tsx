3107e0d3b4efc00ec2ba4b1368be627c41434342
Jordan Lambrecht <jordan@pixelbakery.com>
2023-11-29 00:38:41 -0600

    feat: added print order page

--- components/modules/Nav.tsx
+++ components/modules/Nav.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
+
 function Nav() {
   return (
     <div className='bg-white mb-0 mx-0 pt-12 px-8 lg:px-12'>
       <header className='max-w-7xl mx-auto'>
-        <h1 className='prose prose-zinc text-3xl font-semibold'>
-          Jordan Lambrecht
-        </h1>
+        <div className='flex flex-row justify-between'>
+          <div>
+            <Link href={"/"} hrefLang='us-en'>
+              <span className='prose prose-zinc text-3xl font-semibold prose-h1 display-inline cursor-pointer'>
+                Jordan Lambrecht
+              </span>
+            </Link>
+          </div>
+          <nav className='flex flex-row justify-end gap-x-8 relative items-end'>
+            <div className=' prose prose-zinc  font-bold accent-zinc-200 hover:scale-95 cursor-pointer'>
+              <Link href={"/"} className='relative inline-block cursor-pointer'>
+                <span>Main</span>
+              </Link>
+            </div>
+          </nav>
+        </div>
       </header>
     </div>
   );
---
+++ pages/print-orders.tsx
@@ -0,0 +1,54 @@
+import Section from "@modules/Section";
+import Link from "next/link";
+import { NextSeo, SocialProfileJsonLd } from "next-seo";
+import Script from "next/script";
+
+const Page_PrintOrders = () => {
+  const Form = () => {
+    return (
+      <iframe
+        data-tally-src='https://tally.so/embed/nW8ldP?alignLeft=1&transparentBackground=1&dynamicHeight=1&tallyBranding=0'
+        loading='lazy'
+        width='100%'
+        height='1002'
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+        title='Print Orders'
+      ></iframe>
+    );
+  };
+  return (
+    <>
+      <NextSeo
+        title='Print Orders | Jordan Lambrecht'
+        description={`Step right up and get your prints. Prints here, get ya prints here!`}
+        canonical='https://jordanlambrecht.com/print-orders'
+        openGraph={{
+          url: "https://jordanlambrecht.com/print-orders",
+          title: "Jordan Lambrecht",
+          description:
+            "Step right up and get your prints. Prints here, get ya prints here!",
+          images: [
+            {
+              url: "https://jordanlambrecht.com/img/JordanPlantStore.jpg",
+              width: 1920,
+              height: 1440,
+              alt: "Jordan Lambrecht in Lincoln, Nebraska",
+              type: "image/jpeg",
+            },
+          ],
+          siteName: "Jordan Lambrecht",
+        }}
+      />
+      <Script src='https://tally.so/widgets/embed.js' />
+      <Section id={"print-orders"} className='w-full'>
+        <h1>Print Orders@</h1>
+        <div className='w-full max-w-lg mx-auto'>
+          <Form />
+        </div>
+      </Section>
+    </>
+  );
+};
+export default Page_PrintOrders;

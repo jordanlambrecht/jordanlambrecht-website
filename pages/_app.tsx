ab3b2891ec28264cfee4fd503ab8000d37e59e9c
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-06 16:45:12 -0600

    test: debugging 6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,27 +12,27 @@ const fields = localFont({
   preload: true,
   src: [
     {
-      path: "./fonts//fields/Fields-Regular.woff2",
+      path: "./fonts/fields/Fields-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts//fields/Fields-Medium.woff2",
+      path: "./fonts/fields/Fields-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts//fields/Fields-Bold.woff2",
+      path: "./fonts/fields/Fields-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts//fields/Fields-ExtraBold.woff2",
+      path: "./fonts/fields/Fields-ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "./fonts//fields/Fields-Black.woff2",
+      path: "./fonts/fields/Fields-Black.woff2",
       weight: "900",
       style: "normal",
     },
@@ -45,32 +45,32 @@ const quasimoda = localFont({
   preload: true,
   src: [
     {
-      path: "./fonts//quasimoda/Quasimoda-Regular.woff2",
+      path: "./fonts/quasimoda/Quasimoda-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts//quasimoda/Quasimoda-Medium.woff2",
+      path: "./fonts/quasimoda/Quasimoda-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts//quasimoda/Quasimoda-MediumItalic.woff2",
+      path: "./fonts/quasimoda/Quasimoda-MediumItalic.woff2",
       weight: "500",
       style: "italic",
     },
     {
-      path: "./fonts//quasimoda/Quasimoda-SemiBold.woff2",
+      path: "./fonts/quasimoda/Quasimoda-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts//quasimoda/Quasimoda-SemiBoldItalic.woff2",
+      path: "./fonts/quasimoda/Quasimoda-SemiBoldItalic.woff2",
       weight: "600",
       style: "italic",
     },
     {
-      path: "./fonts//quasimoda/Quasimoda-BoldItalic.woff2",
+      path: "./fonts/quasimoda/Quasimoda-BoldItalic.woff2",
       weight: "700",
       style: "italic",
     },

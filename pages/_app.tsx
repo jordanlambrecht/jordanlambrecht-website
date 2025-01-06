47343a942a66c45257564c78030e66260c8a1d0a
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-06 14:30:22 -0600

    chore: code cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,24 @@
-// import "./styles/typography.css"
 import React from "react"
 import Layout from "@layouts/layout"
 
 import PlausibleProvider from "next-plausible"
 import "./styles/globals.css"
 
+const plausibleConfig = {
+  domain: "jordanlambrecht.com",
+  trackOutboundLinks: true,
+  trackLocalhost: process.env.NODE_ENV !== "production",
+  selfHosted: true,
+  customDomain: "https://analytics.jordy.world",
+  enabled: true,
+}
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <PlausibleProvider
-        domain='jordanlambrecht.com'
-        trackOutboundLinks
-        trackLocalhost={process.env.NODE_ENV !== "production"}
-        selfHosted
-        customDomain='https://analytics.jordy.world'
-        enabled
-      >
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </PlausibleProvider>
-    </>
+    <PlausibleProvider {...plausibleConfig}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleProvider>
   )
 }
